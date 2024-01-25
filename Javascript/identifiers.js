f1630fa6147e1d722acb5c66eb69fe58cb2f817b
ShanborSwer <shanborswer98@gmail.com>
2024-01-25 13:08:22 +0530

    identifier

---
+++ Javascript/identifiers.js
@@ -0,0 +1,29 @@
+var brand = "Peter England";
+var product_name = "Checkered T-shirt";
+var price = 499;
+var rating = 3.7;
+var availability = true;
+var discount  ;
+var location  ;
+
+console.log (brand);
+console.log (product_name)
+console.log (price)
+console.log (rating)
+console.log (availability)
+console.log (discount)
+console .log (location)
+
+console.log (typeof brand);
+console.log (typeof product_name)
+console.log (typeof price)
+console.log (typeof rating)
+console.log (typeof availability)
+console.log (typeof discount)
+console .log (typeof location)
+
+var brand = "Puma";
+console.log (brand);
+
+delete brand;
+console.log (brand);m 
---
+++ Javascript/non-primitive.js
@@ -0,0 +1,22 @@
+var specification = {
+    "Fits" : "Slim",
+    "Size" : "L"
+}
+
+var colors = ['Red', 'Black', 'White']
+var size = [36,37,38,39]
+
+var emp = {
+    "emp_id" : 101,
+    "emp_name" : "Shanbor",
+    "emp_salary" : 50000
+}
+
+console.log (specification);
+console.log (emp);
+console.log (colors);
+console.log (size);
+
+console.log (typeof specification)
+console.log (typeof emp)
+
