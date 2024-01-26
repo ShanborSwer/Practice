aece7167f8505a99a8fcabe4b0b4e7fe4d98742a
ShanborSwer <shanborswer98@gmail.com>
2024-01-26 15:13:55 +0530

    '.

--- Javascript/Operators/ternary.js
+++ Javascript/Operators/ternary.js
@@ -1,3 +1,3 @@
 let sal = 25000;
 
-sal > 40000 ?console.log ("Eligible for marriage") : console.log ("Not eligible for marriage");
+sal > 40000 ? console.log ("Eligible for marriage") : console.log ("Not eligible for marriage");
