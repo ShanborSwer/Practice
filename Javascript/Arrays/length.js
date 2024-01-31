4855c99a03ad363d4162c3a0eb383d14fb5bc794
ShanborSwer <shanborswer98@gmail.com>
2024-01-31 12:53:34 +0530

    .

---
+++ Javascript/Arrays/length.js
@@ -0,0 +1,6 @@
+let enames=['Rahul', 'Priya', 'Sonia', 'Sunil', 'Rajesh', 'Shanbor']
+let i = 0;
+while (i<=5){
+    console.log(enames[i]);
+    i++;
+}
---
+++ Javascript/Arrays/read.js
@@ -0,0 +1,11 @@
+let enames=['rahul','sonia','priya','modi','swah','dhone','rajsh','madan']
+console.log(enames.length)
+
+console.log(enames[0])
+console.log(enames[1])
+console.log(enames[2])
+console.log(enames[3])
+console.log(enames[4])
+console.log(enames[5])
+console.log(enames[6])
+console.log(enames[7])
