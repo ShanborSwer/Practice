df59b223d957c45217255bb6c2845ffe8e421df3
ShanborSwer <shanborswer98@gmail.com>
2024-01-31 13:03:43 +0530

    .

---
+++ Javascript/Arrays/forof.js
@@ -0,0 +1,15 @@
+let enames=['Rahul', 'Priya', 'Sonia', 'Sunil', 'Rajesh', 'Shanbor'];
+let ids = [101,102,103,104];
+let sizes = ['S','M','L','XL','XXL'];
+
+for (size of sizes){
+    console.log(size);
+}
+
+for (ename of enames){
+    console.log (ename);
+}
+
+for (id of ids){
+    console.log(id);
+}
