8452c9adb1c43ce0398d7a2e3dcf299889465216
ShanborSwer <shanborswer98@gmail.com>
2024-01-31 13:08:54 +0530

    .

---
+++ Javascript/Arrays/empty.js
@@ -0,0 +1,12 @@
+let ids = []
+let uids=[101,102,103,104]
+
+console.log(ids.length);
+console.log(uids.length);
+
+if (uids.length>0){
+    console.log("Not an empty array");
+}
+else{
+    console.log("Empty array");
+}
---
+++ Javascript/Arrays/len.js
@@ -0,0 +1,3 @@
+let users=[101,"Rahul",true,{},10,1020,10]
+
+console.log (users.length);
