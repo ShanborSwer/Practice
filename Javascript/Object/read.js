4abbbecebfa34214336bd37c8e02741059c01a4c
ShanborSwer <shanborswer98@gmail.com>
2024-02-01 11:58:29 +0530

    read

---
+++ Javascript/Object/read.js
@@ -0,0 +1,11 @@
+let emp = {
+    id : 101,
+    name : "Rahul",
+    email : "rahul@gmail.com",
+    salary : 45000
+}
+
+console.log(emp.id);
+console.log(emp.name);
+console.log(emp.email);
+console.log(emp.salary);
