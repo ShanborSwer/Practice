9016b2736d426d5a6c8d22e7d39c9c8017024de0
ShanborSwer <shanborswer98@gmail.com>
2024-02-01 12:14:28 +0530

    for

---
+++ Javascript/Object/user.js
@@ -0,0 +1,9 @@
+let users = [
+    {id :101, name :"Rahul", salary : 45000},
+    {id :102, name :"Priyanka", salary : 55000},
+    {id :101, name :"Sonia", salary : 65000}
+]
+
+for (i=0;i<=users.length-1;i++){
+    console.log(users[i].id);
+}
