86eee345ffe17f8cf41e6947329d3656bc683d05
ShanborSwer <shanborswer98@gmail.com>
2024-02-05 13:32:36 +0530

    .

--- Javascript/Arrays/Array Methods/arrunshift.js
+++ Javascript/Arrays/Array Methods/arrunshift.js
@@ -3,4 +3,4 @@ let ids=[101,102,103,104,105]
 ids.unshift(100)
 console.log(ids)
 
-// ushift will add an element to the beginning of the array
+// unshift will add an element to the beginning of the array
