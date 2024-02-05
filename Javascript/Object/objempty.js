cd1b01f58394630fe9ed12bd86598eae43352bef
ShanborSwer <shanborswer98@gmail.com>
2024-02-05 12:51:03 +0530

    empty

--- Javascript/Object/objempty.js
+++ Javascript/Object/objempty.js
@@ -1,4 +1,12 @@
 let emp = {
     eid : 101,
-    eName : "Shan"
+    eName : "Shan",
+    eSal : 45000
+}
+
+if (Object.keys(emp).length>0){
+    console.log("Object is not empty");
+}
+else {
+    console.log("Object is empty");
 }
