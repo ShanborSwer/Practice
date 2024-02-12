e01b5c7fd2295c430506b558a8f8cf7979f7cb0f
ShanborSwer <shanborswer98@gmail.com>
2024-02-12 13:01:20 +0530

    dig

---
+++ Javascript/Functions/examples/name.js
@@ -0,0 +1,7 @@
+let string = "sHAnboR"
+let fullChar = string.slice(0,1).toUpperCase()+string.slice(1,string.length).toLowerCase();
+// var upperChar = firstChar.toUpperCase();
+// var restOfChar = name.slice(1,name.length);
+// var restOfChar = restOfChar.toLowerCase();
+
+console.log(fullChar);
---
+++ Javascript/Functions/examples/upper.js
@@ -0,0 +1,3 @@
+let string = "heLLo";
+string = string.charAt(0).toUpperCase()+string.slice(1).toLowerCase();
+console.log(string);
