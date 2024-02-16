deba15028f9b7f26cea1ce484027820517240eea
ShanborSwer <shanborswer98@gmail.com>
2024-02-16 13:06:58 +0530

    es6

---
+++ Javascript/ES6/newids.js
@@ -0,0 +1,8 @@
+let ids = [101,102,103,104]
+let new_ids = ids;
+
+console.log(ids);
+console.log(new_ids)
+
+new_ids[0]="Rahul";
+console.log(new_ids)
---
+++ Javascript/ES6/push.js
@@ -0,0 +1,8 @@
+let ids=[101,102,103,104]
+
+let new_Ids=[];
+
+for (id in ids){
+    new_Ids.push(ids)
+}
+console.log(new_Ids[id])
