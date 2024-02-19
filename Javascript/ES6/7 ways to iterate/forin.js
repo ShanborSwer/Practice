d29083f4bc0857811208550194071ad5bff2085b
ShanborSwer <shanborswer98@gmail.com>
2024-02-19 12:48:20 +0530

    forin

---
+++ Javascript/ES6/7 ways to iterate/forin.js
@@ -0,0 +1,11 @@
+let eids = [101,102,103,104,105]
+
+let emp={id:101,name:"Shanbor",salary:55000}
+
+for (eid in eids){
+    console.log(eids[eid]);
+}
+
+for (prop in emp){
+    console.log(emp[prop])
+}
