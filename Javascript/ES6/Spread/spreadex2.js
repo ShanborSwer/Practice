c9b6be80048b84fb3e4a12b0c705029b7d39e5a9
ShanborSwer <shanborswer98@gmail.com>
2024-02-20 12:20:34 +0530

    spread

---
+++ Javascript/ES6/Spread/spreadex2.js
@@ -0,0 +1,17 @@
+let a = [20,30,40];
+let b = [20,30,40,50];
+
+let c = [...a , ...b];
+
+/* let c = [];
+
+for (ele of a){
+    c.push(ele);
+}
+
+for (ele of b){
+    c.push(ele);
+}
+*/
+
+console.log(c);
