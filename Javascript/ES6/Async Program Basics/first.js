f0075bbb8deade71c9a78f36693682bb9c7a8f73
ShanborSwer <shanborswer98@gmail.com>
2024-02-21 13:06:12 +0530

    async

---
+++ Javascript/ES6/Async Program Basics/first.js
@@ -0,0 +1,22 @@
+setTimeout(()=>{
+    console.log("Request 1")
+},10000)
+setTimeout(()=>{
+    console.log("Request 2")
+},8000)
+setTimeout(()=>{
+    console.log("Request 3")
+},6000)
+setTimeout(()=>{
+    console.log("Request 4")
+},4000)
+setTimeout(()=>{
+    console.log("Request 5")
+},3000)
+setTimeout(()=>{
+    console.log("Request 6")
+},1000)
+
+
+
+
