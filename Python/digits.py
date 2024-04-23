78b0d1187a5d06bc53dc824062b5c5c8459cdff0
ShanborSwer <shanborswer98@gmail.com>
2024-04-23 11:41:42 +0530

    py

---
+++ Python/digits.py
@@ -0,0 +1,7 @@
+def three_dig(num):
+    if (num >=100 and num<=999):
+        print("It is a three digit number")
+    else:
+        print("It is not a three digit number")
+        
+three_dig(3454)
