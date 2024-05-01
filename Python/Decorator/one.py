c2fed5e9d7c59217f5ad3493a856cfcc32d96aa9
ShanborSwer <shanborswer98@gmail.com>
2024-05-01 12:22:32 +0530

    py

---
+++ Python/Decorator/one.py
@@ -0,0 +1,21 @@
+def home_page(name,is_login):
+    print("Home Page")
+    
+def order_page(name,is_login):
+    print("Order Page")
+    
+def product_page(name,is_login):
+    print("Product Page")
+    
+def profile_page(name,is_login):
+    print("Profile Page")
+    
+home_page("Shanbor",False)
+order_page("Shanbor",False)
+product_page("Shanbor",False)
+profile_page("Shanbor",False)
+
+home_page("Sunil",True)
+order_page("Sunil",True)
+product_page("Sunil",True)
+profile_page("Sunil",True)
