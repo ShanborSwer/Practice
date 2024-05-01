025c7743a00026bcca087922c5f79bcc1e7e4db0
ShanborSwer <shanborswer98@gmail.com>
2024-05-01 11:09:53 +0530

    py

---
+++ Python/modules/choice.py
@@ -0,0 +1,6 @@
+from random import choice
+
+enames = ['Shanbor','Sunil','Rajesh','Seerisha','Shruti','Naina']
+
+for x in range(10):
+    print(choice(enames))
---
+++ Python/modules/pi.py
@@ -0,0 +1,4 @@
+#import math
+from math import pi as value
+
+print(value)
